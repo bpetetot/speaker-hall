520e86110621b4ad483d4bf00f9624263b9a2387
Benjamin Petetot <bpetetot@gmail.com>
2017-11-04 15:51:26 +0100

    styling login screen

--- src/screens/login/login.jsx
+++ src/screens/login/login.jsx
@@ -5,10 +5,19 @@ import IconLabel from '../../components/iconLabel'
 import './login.css'
 
 const Login = ({ signin, nextUrl }) => (
-  <div className="home-login">
-    <button className="btn btn-primary" onClick={() => signin('google', nextUrl)}>
-      <IconLabel icon="fa fa-google" label="Login with Google" />
-    </button>
+  <div className="login">
+    <h1>Connect to Conference Hall</h1>
+    <div>
+      <button className="btn btn-google" onClick={() => signin('google', nextUrl)}>
+        <IconLabel icon="fa fa-google" label="With Google" />
+      </button>
+      <button className="btn btn-twitter" onClick={() => signin('twitter', nextUrl)}>
+        <IconLabel icon="fa fa-twitter" label="With Twitter" />
+      </button>
+      <button className="btn btn-github" onClick={() => signin('github', nextUrl)}>
+        <IconLabel icon="fa fa-github" label="With Github" />
+      </button>
+    </div>
   </div>
 )
 
